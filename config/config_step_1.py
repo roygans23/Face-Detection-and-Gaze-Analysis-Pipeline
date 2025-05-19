0dbcad581884b69391dfe4f35519e0c112d7cfc5
Roy Gans TAU Developer <roygans@mai.tau.ac.il>
2025-05-19 10:08:30 +0300

    Change to relative path of config constants (instead of hardcoded absolute that can break code) in config_step_1.py

--- config/config_step_1.py
+++ config/config_step_1.py
@@ -7,8 +7,8 @@ FACES_FOLDER = "data/faces"
 CSV_OUTPUTS = "data/csvs_outputs"  # For generated CSVs
 
 # --- MODEL FILES (EDIT THESE TO MATCH YOUR SYSTEM) ---
-PROTOTXT_PATH = r"C:\Users\IsarShmueli\Korean_Modulate\utils\deploy.prototxt.txt"
-CAFFE_MODEL_PATH = r"C:\Users\IsarShmueli\Korean_Modulate\utils\res10_300x300_ssd_iter_140000.caffemodel"
+PROTOTXT_PATH = r"utils\deploy.prototxt.txt"
+CAFFE_MODEL_PATH = r"utils\res10_300x300_ssd_iter_140000.caffemodel"
 
 # --- FACE DETECTION THRESHOLDS ---
 CONF_THRESHOLD = 0.7
